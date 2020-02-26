b216d9cb1d98d536a78930201774a78df7ea9b1c
HENRY BENAYE <2106747@talnet.nl>
2020-02-26 13:11:11 +0100

    Uitwerking van de exercise

--- cart.php
+++ cart.php
@@ -10,7 +10,7 @@
     <h1>Basketbal (#2)</h2>
     <h1>Skeelers (#3)</h3>
     <form action="cart.php" method="get">
-        Keuze: <input type="text" name="keuze"> 
+        Keuze: <input type="number" name="keuze" min="1" max="3"> 
         <input type="submit">
     </form>
     
@@ -19,7 +19,7 @@
 
 <?php   
 session_start();
-if(isset($_GET["keuze"])) {
+if(isset($_GET["keuze"]) || $_GET["keuze"] <= 3) {
     $_SESSION['keuze'] = $_GET["keuze"];
     echo"Gekozen item:". $_SESSION["keuze"];
    
